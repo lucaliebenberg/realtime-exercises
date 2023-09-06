0aab0446c15e909145344cc2797f5000ed57221c
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-09-06 10:29:08 +0200

    project update

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -1,25 +1,23 @@
 const chat = document.getElementById("chat");
 const msgs = document.getElementById("msgs");
 
-// let's store all current messages here
+// this will hold all the most recent messages
 let allChat = [];
 
-// the interval to poll at in milliseconds
 const INTERVAL = 3000;
 
-// a submit listener on the form in the HTML
 chat.addEventListener("submit", function (e) {
   e.preventDefault();
   postNewMsg(chat.elements.user.value, chat.elements.text.value);
   chat.elements.text.value = "";
 });
-
 async function postNewMsg(user, text) {
   const data = {
     user,
     text,
   };
 
+  // request options
   const options = {
     method: "POST",
     body: JSON.stringify(data),
@@ -28,7 +26,9 @@ async function postNewMsg(user, text) {
     },
   };
 
-  await fetch("/poll", options);
+  // send POST request
+  const res = await fetch("/poll", options);
+  const json = await res.json();
 }
 
 async function getNewMsgs() {
@@ -36,27 +36,43 @@ async function getNewMsgs() {
   try {
     const res = await fetch("/poll");
     json = await res.json();
+
+    if (res.status >= 400) {
+      throw new Error("requesr did not succeed" + res.status);
+    }
+
+    allChat = json.msg;
+    render();
+    failedTries = 0;
+    setTimeout(getNewMsgs, INTERVAL);
   } catch (e) {
-    // backoff code
+    // back off could would go here
     console.error("polling error", e);
+    failedTries++;
   }
-  allChat = json.msg;
-  render();
-  setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
-  // as long as allChat is holding all current messages, this will render them
-  // into the ui. yes, it's inefficent. yes, it's fine for this example
+  // todo
   const html = allChat.map(({ user, text, time, id }) =>
     template(user, text, time, id)
   );
   msgs.innerHTML = html.join("\n");
 }
 
-// given a user and a msg, it returns an HTML string to render to the UI
 const template = (user, msg) =>
   `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
-// make the first request
-getNewMsgs();
+const BACKOFF = 5000;
+let timeToMakeNextRequest = 0;
+let failedTries = 0;
+async function rafTimer(time) {
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+    timeToMakeNextRequest = time + INTERVAL + failedTries + BACKOFF;
+  }
+
+  requestAnimationFrame(rafTimer);
+}
+
+requestAnimationFrame(rafTimer);
